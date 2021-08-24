61ddd9fe3e1c9a97f007053f2abac630c8316223
Resul Takak <resultakak@gmail.com>
2021-08-24 12:07:18 +0300

    Create problem.py

---
+++ 9_Song/problem.py
@@ -0,0 +1,21 @@
+class Song:
+    def __init__(self, name):
+        self.name = name
+        self.next = None
+
+    def next_song(self, song):
+        self.next = song 
+    
+    def is_repeating_playlist(self):
+        """
+        :returns: (bool) True if the playlist is repeating, False if not.
+        """
+        return None
+            
+first = Song("Hello")
+second = Song("Eye of the tiger")
+    
+first.next_song(second)
+second.next_song(first)
+    
+print(first.is_repeating_playlist())
