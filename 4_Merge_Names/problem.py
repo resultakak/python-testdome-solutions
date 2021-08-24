71a20d72850e5bb9b1f56e26ca02f13ce9944df2
Resul Takak <resultakak@gmail.com>
2021-08-24 11:48:26 +0300

    Create problem.py

---
+++ 4_Merge_Names/problem.py
@@ -0,0 +1,7 @@
+def unique_names(names1, names2):
+    return None
+
+if __name__ == "__main__":
+    names1 = ["Ava", "Emma", "Olivia"]
+    names2 = ["Olivia", "Sophia", "Emma"]
+    print(unique_names(names1, names2)) # should print Ava, Emma, Olivia, Sophia
