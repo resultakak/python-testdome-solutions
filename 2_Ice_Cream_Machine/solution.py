6382b885567c3f34de0d14f227659867a25832f2
Resul Takak <resultakak@gmail.com>
2021-08-24 11:39:08 +0300

    Create solution.py

---
+++ 2_Ice_Cream_Machine/solution.py
@@ -0,0 +1,12 @@
+class IceCreamMachine:
+
+    def __init__(self, ingredients, toppings):
+        self.ingredients = ingredients
+        self.toppings = toppings
+
+    def scoops(self):
+        return [[ingredient, topping] for ingredient in self.ingredients for topping in self.toppings]
+
+if __name__ == "__main__":
+    machine = IceCreamMachine(["vanilla", "chocolate"], ["chocolate sauce"])
+    print(machine.scoops())  # should print[['vanilla', 'chocolate sauce'], ['chocolate', 'chocolate sauce']]
