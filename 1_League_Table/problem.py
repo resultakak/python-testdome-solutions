2951a2947cc8d8f75f4c74b38020331d8d8eae12
Resul Takak <resultakak@gmail.com>
2021-08-24 11:30:28 +0300

    Create problem.py

---
+++ 1_League_Table/problem.py
@@ -0,0 +1,21 @@
+from collections import Counter
+from collections import OrderedDict
+
+class LeagueTable:
+    def __init__(self, players):
+        self.standings = OrderedDict([(player, Counter()) for player in players])
+       
+    def record_result(self, player, score):
+        self.standings[player]['games_played'] += 1
+        self.standings[player]['score'] += score
+      
+    def player_rank(self, rank):
+        return None
+
+if __name__ == "__main__":
+    table = LeagueTable(['Mike', 'Chris', 'Arnold'])
+    table.record_result('Mike', 2)
+    table.record_result('Mike', 3)
+    table.record_result('Arnold', 5)
+    table.record_result('Chris', 5)
+    print(table.player_rank(1))
